5b1bf9cd61961d8499b2e6dc99913dfe0f296d70
Laurence Castaneda <laurence.d.castaneda@gmail.com>
2020-08-24 17:35:27 -0500

    program logic for optional form inputs

---
+++ public/assets/js/apply.js
@@ -0,0 +1,43 @@
+// Configure logic to toggle inputs when different from student info
+
+
+// module to reference checkboxes and containers
+
+const form = (() => {
+  // reference checkboxes
+  const parentAddressCheckbox = document.getElementById('sameaddress');
+  const tuitionPatronCheckbox = document.getElementById('sametuition');
+
+  // reference input containers
+  const parentAddress = document.getElementById('paddressContainer');
+  const tuitionPatronFirst = document.getElementById('tFirstName');
+  const tuitionPatronLast = document.getElementById('tLastName');
+  const tuitionAddress = document.getElementById('taddressContainer');
+  const tuitionPhoneEmail = document.getElementById('tuitionInfo');
+
+  return {
+    parentAddressCheckbox,
+    tuitionPatronCheckbox,
+    parentAddress,
+    tuitionPatronFirst,
+    tuitionPatronLast,
+    tuitionAddress,
+    tuitionPhoneEmail,
+  }
+})();
+
+// module to handle logic
+
+const handleCheckbox = (() => {
+  form.parentAddressCheckbox.addEventListener('click', () => {
+    form.parentAddress.toggleAttribute('hidden');
+  });
+
+  form.tuitionPatronCheckbox.addEventListener('click', () => {
+    form.tuitionPatronFirst.toggleAttribute('hidden');
+    form.tuitionPatronLast.toggleAttribute('hidden');
+    form.tuitionPhoneEmail.toggleAttribute('hidden');
+  });
+})();
+
+handleCheckbox();
